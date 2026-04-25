33e1eb47ef8cd46ee49a16d7d4ee95b3ccad89f4
Slimaart <152856456+Slimaart@users.noreply.github.com>
2026-04-25 18:43:52 +0200

    Update IntelPage.vue
    Delete of the TopNavBar element which is duplicated since we already have it in AuthLayout.vue

--- the_heist/frontend/src/pages/IntelPage.vue
+++ the_heist/frontend/src/pages/IntelPage.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { computed, ref } from 'vue'
-import TopNavbar from '../components/layout/TopNavbar.vue'
 import BackgroundGlow from '../components/ui/BackgroundGlow.vue'
 import PageTransition from '../components/ui/PageTransition.vue'
 import StatCard from '../components/ui/StatCard.vue'
@@ -86,7 +85,6 @@ function openLogoutModal() {
 <template>
   <div class="min-h-screen bg-[#05070d] bg-gradient-to-b from-[#06080f] to-[#04060b]">
     <BackgroundGlow />
-    <TopNavbar :on-sign-out-click="openLogoutModal" />
 
     <PageTransition>
       <main class="mx-auto max-w-[1400px] px-6 py-8 lg:px-8">
